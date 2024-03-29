32bf95b534745f20185a215ba0a29ea4c190cc93
Stanislav_Yurchenko <yurchenko.stas15@gmail.com>
2024-03-29 16:39:52 +0500

    Первый тест cytoscape

--- pages/org-structure.py
+++ pages/org-structure.py
@@ -1,5 +1,6 @@
 import dash
 from dash import html
+import dash_cytoscape as cyto
 
 dash.register_page(
     __name__,
@@ -7,8 +8,48 @@ dash.register_page(
     title='org-structure page',
     name='org-structure page'
 )
+nodes = [
+    {
+        'data': {'id': short, 'label': label},
+        'position': {'x': 20 * lat, 'y': -20 * long}
+    }
+    for short, label, long, lat in (
+        ('la', 'Los Angeles', 34.03, -118.25),
+        ('nyc', 'New York', 40.71, -74),
+        ('to', 'Toronto', 43.65, -79.38),
+        ('mtl', 'Montreal', 45.50, -73.57),
+        ('van', 'Vancouver', 49.28, -123.12),
+        ('chi', 'Chicago', 41.88, -87.63),
+        ('bos', 'Boston', 42.36, -71.06),
+        ('hou', 'Houston', 29.76, -95.37)
+    )
+]
+
+edges = [
+    {'data': {'source': source, 'target': target}}
+    for source, target in (
+        ('van', 'la'),
+        ('la', 'chi'),
+        ('hou', 'chi'),
+        ('to', 'mtl'),
+        ('mtl', 'bos'),
+        ('nyc', 'bos'),
+        ('to', 'hou'),
+        ('to', 'nyc'),
+        ('la', 'nyc'),
+        ('nyc', 'bos')
+    )
+]
+
+elements = nodes + edges
+graph_elements = nodes + edges
 
 layout = html.Div([
     html.H1('This is our org-structure page'),
-    html.Div('This is our org-structure page content.'),
+    cyto.Cytoscape(
+        id='cytoscape-two-nodes',
+        layout={'name': 'preset'},
+        style={'width': '100%', 'height': '400px'},
+        elements=graph_elements
+    ),
 ])
