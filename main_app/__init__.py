243736bf4fefbb9a4134bd57b43db7455da37763
Amber0444 <alexander.shevelev04@gmail.com>
2024-04-06 11:24:39 +0500

    почти работающая система авторизации

--- main_app/__init__.py
+++ main_app/__init__.py
@@ -1,18 +1,19 @@
 """Initialize Flask app."""
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
-def init_app():
-    """Construct core Flask application with embedded Dash app."""
-    app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.Config')
+app = Flask(__name__, instance_relative_config=False)
+app.config.from_object('config.Config')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+db = SQLAlchemy(app)
+manager = LoginManager(app)
 
-    with app.app_context():
-        # Import parts of our core Flask app
-        from . import routes
+with app.app_context():
+    # Import parts of our core Flask app
+    from . import routes
 
-        # Import Dash application
-        from .dash_apps.organization_graph.dashboard import init_dashboard
-        app = init_dashboard(app)
-
-        return app
+    # Import Dash application
+    from .dash_apps.organization_graph.dashboard import init_dashboard
+    app = init_dashboard(app)
