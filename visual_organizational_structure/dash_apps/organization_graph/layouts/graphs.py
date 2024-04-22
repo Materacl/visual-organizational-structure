7366cb4210780325cc7bfbd05de6d598f643ce51
Materacl <yurchenko.stas15@gmail.com>
2024-04-22 14:11:51 +0500

    Немного поменял граф

--- visual_organizational_structure/dash_apps/organization_graph/layouts/graphs.py
+++ visual_organizational_structure/dash_apps/organization_graph/layouts/graphs.py
@@ -1,5 +1,86 @@
 import dash_cytoscape as cyto
 
+stylesheet1 = [
+    {
+        'selector': 'node',  # Style for nodes
+        'style': {
+            'label': 'data(label)',  # Updated to use 'label' from node data
+            'text-valign': 'center',  # Center the text vertically
+            'text-halign': 'center',  # Align the text to the center
+            'text-margin-y': '0px',  # Adjust vertical text margin
+            'text-margin-x': '0px',  # Adjust horizontal text margin
+            'background-color': '#1f77b4',  # Change node color to a blue shade
+            'color': 'white',  # Change text color to white
+            'shape': 'roundrectangle',  # Use roundrectangle shape for nodes
+            'width': 'label',  # Set node width based on label size
+            'height': 'label',  # Set node height based on label size
+            'font-size': '16px',  # Adjust font size
+            'padding': '10px',  # Add padding to node
+            'text-wrap': 'wrap'  # Allow text to wrap within node
+        }
+    },
+    {
+        'selector': 'edge',  # Style for edges
+        'style': {
+            'width': 3,
+            'line-color': '#ccc',
+            'target-arrow-color': '#ccc',
+            'target-arrow-shape': 'triangle'
+        }
+    }
+]
+
+stylesheet2 = [
+    {
+        'selector': 'node',  # Style for nodes
+        'style': {
+            'label': 'data(label)',  # Updated to use 'label' from node data
+            'text-valign': 'center',  # Center the text vertically
+            'text-halign': 'center',  # Align the text to the center
+            'text-margin-y': '0px',  # Adjust vertical text margin
+            'text-margin-x': '0px',  # Adjust horizontal text margin
+            'background-color': '#1f77b4',  # Change node color to a blue shade
+            'color': 'white',  # Change text color to white
+            'shape': 'roundrectangle',  # Use roundrectangle shape for nodes
+            'width': 'label',  # Set node width based on label size
+            'height': 'label',  # Set node height based on label size
+            'font-size': '16px',  # Adjust font size
+            'padding': '10px',  # Add padding to node
+            'text-wrap': 'wrap'  # Allow text to wrap within node
+        }
+    },
+    {
+        'selector': '.nonterminal',
+        'style': {
+            'label': 'data(confidence)',
+            'background-opacity': 0,
+            "text-halign": "left",
+            "text-valign": "top",
+        }
+    },
+    {
+        'selector': '.support',
+        'style': {'background-opacity': 0}
+    },
+    {
+        'selector': 'edge',
+        'style': {
+            "source-endpoint": "inside-to-node",
+            "target-endpoint": "inside-to-node",
+        }
+    },
+    {
+        'selector': '.terminal',
+        'style': {
+            'label': 'data(name)',
+            'width': 10,
+            'height': 10,
+            "text-valign": "center",
+            "text-halign": "right",
+            'background-color': '#222222'
+        }
+    }
+]
 
 def get_tree_graph(graph_roots=None, graph_elements=None):
     """
@@ -18,24 +99,9 @@ def get_tree_graph(graph_roots=None, graph_elements=None):
         layout={
             'name': 'breadthfirst',  # Use 'breadthfirst' layout algorithm
             'roots': '[id = "LE0"]',  # Root node
-            'idealEdgeLength': 100,
-            'nodeOverlap': 20,
-            'refresh': 20,
-            'fit': True,
-            'padding': 30,
-            'randomize': False,
-            'componentSpacing': 100,
-            'nodeRepulsion': 400000,
-            'edgeElasticity': 100,
-            'nestingFactor': 5,
-            'gravity': 80,
-            'numIter': 1000,
-            'initialTemp': 200,
-            'coolingFactor': 0.95,
-            'minTemp': 1.0
         },
         # Zoom configuration
-        minZoom=0.5,
+        minZoom=0.1,
         maxZoom=5,
         # Enable box selection
         boxSelectionEnabled=True,
@@ -52,33 +118,5 @@ def get_tree_graph(graph_roots=None, graph_elements=None):
         # Graph elements
         elements=graph_elements,
         # Stylesheet configuration
-        stylesheet=[
-            {
-                'selector': 'node',  # Style for nodes
-                'style': {
-                    'label': 'data(label)',  # Updated to use 'label' from node data
-                    'text-valign': 'center',  # Center the text vertically
-                    'text-halign': 'center',  # Align the text to the center
-                    'text-margin-y': '0px',  # Adjust vertical text margin
-                    'text-margin-x': '0px',  # Adjust horizontal text margin
-                    'background-color': '#1f77b4',  # Change node color to a blue shade
-                    'color': 'white',  # Change text color to white
-                    'shape': 'roundrectangle',  # Use roundrectangle shape for nodes
-                    'width': 'label',  # Set node width based on label size
-                    'height': 'label',  # Set node height based on label size
-                    'font-size': '16px',  # Adjust font size
-                    'padding': '10px',  # Add padding to node
-                    'text-wrap': 'wrap'  # Allow text to wrap within node
-                }
-            },
-            {
-                'selector': 'edge',  # Style for edges
-                'style': {
-                    'width': 3,
-                    'line-color': '#ccc',
-                    'target-arrow-color': '#ccc',
-                    'target-arrow-shape': 'triangle'
-                }
-            }
-        ]
+        stylesheet=stylesheet2
     )
