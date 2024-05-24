24c3bef18e2f231e539f0880e14f69155c7d238b
Materacl <yurchenko.stas15@gmail.com>
2024-05-24 17:12:34 +0500

    Много оптимизации и использование redis для кэширования

--- visual_organizational_structure/dash_apps/organization_graph/layouts/org_structure_graph/__init__.py
+++ visual_organizational_structure/dash_apps/organization_graph/layouts/org_structure_graph/__init__.py
@@ -1,11 +1,16 @@
 import json
+import time
+
 import dash_cytoscape as cyto
-from dash import callback, Input, Output, State
+from dash import callback, Input, Output, State, clientside_callback
 from dash.exceptions import PreventUpdate
 from visual_organizational_structure.models import Dashboard
 import visual_organizational_structure.dash_apps.organization_graph.layouts.csv_uploader as csv_uploader
 import visual_organizational_structure.dash_apps.organization_graph.layouts.org_structure_graph.style as style
 from visual_organizational_structure import db
+from visual_organizational_structure.utils import cache
+
+TIMEOUT = 60
 
 # Load extra layouts
 cyto.load_extra_layouts()
@@ -59,22 +64,23 @@ def get_tree_graph(graph_elements=None, roots=None):
 
 @callback(
     Output('cytoscape-org-graph', 'elements'),
-    Input('dashboard-graph-data', 'data'),
+    Input('dashboard-graph-elements', 'data'),
     State("dashboard-general-data", 'data'),
     prevent_initial_call=True
 )
-def update_graph(dashboard_graph_data: dict, dashboard_general_data: dict):
-    elements = dashboard_graph_data.get("graph_elements", [])
+@cache.memoize(timeout=TIMEOUT)
+def update_graph(dashboard_graph_elements: list, dashboard_general_data: dict):
     dashboard = Dashboard.query.get(dashboard_general_data["dashboard_id"])
-    if dashboard.graph_data != json.dumps(elements):
-        dashboard.graph_data = json.dumps(elements)
+    if dashboard.graph_data != json.dumps(dashboard_graph_elements):
+        dashboard.graph_data = json.dumps(dashboard_graph_elements)
         db.session.commit()
-    return elements
+    return dashboard_graph_elements
 
 
 @callback(
     [
-        Output('dashboard-graph-data', 'data', allow_duplicate=True),
+        Output('dashboard-graph-elements', 'data', allow_duplicate=True),
+        Output('dashboard-graph-data', 'data'),
         Output("uploader-csv", "is_open", allow_duplicate=True)
     ],
     Input("confirm-csv-uploader", 'n_clicks'),
@@ -82,14 +88,15 @@ def update_graph(dashboard_graph_data: dict, dashboard_general_data: dict):
         State('uploader-element', 'contents'),
         State("dashboard-general-data", 'data')
     ],
-    prevent_initial_call=True
+    prevent_initial_call=True,
+    running=[(Output("confirm-csv-uploader", "disabled"), True, False)]
 )
+@cache.memoize(timeout=TIMEOUT)
 def handle_csv_uploader(upload_confirm_clicks, uploader_contents, dashboard_general_data):
     graph_tree = csv_uploader.get_data_from_scv(uploader_contents)
     graph_elements = graph_tree.find_by_id('MAIN', method='bfs').get_elements(recursion=False)
     if graph_elements:
         dashboard_graph_data = {
-            "graph_elements": graph_elements,
             "id_to_data": graph_tree.create_index_with_data(),
             "id_to_parent": graph_tree.create_index_with_parents(),
             "id_to_children": graph_tree.create_index_with_ids()
@@ -102,78 +109,80 @@ def handle_csv_uploader(upload_confirm_clicks, uploader_contents, dashboard_gene
         dashboard.id_to_parent = json.dumps(dashboard_graph_data["id_to_parent"])
         dashboard.id_to_children = json.dumps(dashboard_graph_data["id_to_children"])
         db.session.commit()
-        return dashboard_graph_data, False
+        return graph_elements, dashboard_graph_data, False
     else:
         raise PreventUpdate
 
 
 @callback(
-    [
-        Output('dashboard-graph-data', 'data', allow_duplicate=True),
-        Output('dashboard-graph-data', 'pan'),
-    ],
+    Output('dashboard-graph-elements', 'data', allow_duplicate=True),
     Input('search-confirm', 'n_clicks'),
     [
         State('search-input', 'value'),
+        State('dashboard-graph-elements', 'data'),
         State('dashboard-graph-data', 'data')
     ],
-    prevent_initial_call=True
+    prevent_initial_call=True,
+    running=[(Output("search-confirm", "disabled"), True, False)]
 )
-def handle_search(search_confirm_clicks, search_value, dashboard_graph_data):
-    if search_value:
-        id_to_data = dashboard_graph_data.get("id_to_data", {})
-        id_to_parent = dashboard_graph_data.get("id_to_parent", {})
-        if not id_to_data:
-            raise PreventUpdate
-        else:
-            elements = []
-            current_id = search_value
-            while current_id:
-                search_data = id_to_data.get(current_id, [])
-                elements.extend(search_data)
-                current_id = id_to_parent.get(current_id, '')
-
-            current_elements = dashboard_graph_data.get("graph_elements", [])
-            dashboard_graph_data["graph_elements"] = current_elements + elements
-            return dashboard_graph_data, {"x": 200, "y": 200}
-    else:
+@cache.memoize(timeout=TIMEOUT)
+def handle_search(search_confirm_clicks, search_value, current_elements, dashboard_graph_data):
+    if not search_value:
         raise PreventUpdate
+
+    id_to_data = dashboard_graph_data.get("id_to_data", {})
+    id_to_parent = dashboard_graph_data.get("id_to_parent", {})
+
+    if not id_to_data:
+        raise PreventUpdate
+
+    elements = []
+    current_id = search_value
+    while current_id:
+        search_data = id_to_data.get(current_id, [])
+        elements.extend(search_data)
+        current_id = id_to_parent.get(current_id, '')
+
+    return current_elements + elements
 
 
 @callback(
-    Output('dashboard-graph-data', 'data', allow_duplicate=True),
-    Input('cytoscape-org-graph', 'tapNode'),
-    State('dashboard-graph-data', 'data'),
-    prevent_initial_call=True
+    Output('dashboard-graph-elements', 'data', allow_duplicate=True),
+    Input('cytoscape-org-graph', 'tapNodeData'),
+    [
+        State('dashboard-graph-elements', 'data'),
+        State('dashboard-graph-data', 'data'),
+    ],
+    prevent_initial_call=True,
+    running=[(Output("cytoscape-org-graph", "disabled"), True, False)]
 )
-def handle_tap_node(tap_node_data, dashboard_graph_data):
-    if tap_node_data:
-        node_id = tap_node_data['data']['id']
-        id_to_data = dashboard_graph_data.get("id_to_data", {})
-        id_to_children = dashboard_graph_data.get("id_to_children", {})
-
-        tap_elements = id_to_data.get(node_id, [])
-        if len(tap_elements) > 1:
-            current_elements = dashboard_graph_data.get("graph_elements", [])
-            if all(elem in current_elements for elem in tap_elements):
-                children_ids = get_children_ids(node_id, id_to_children)
-                elements_to_remove = [elem for cid in children_ids for elem in id_to_data.get(cid, [])]
-                elements_to_remove.pop(0)
-                new_graph_elements = [elem for elem in current_elements if elem not in elements_to_remove]
-                dashboard_graph_data["graph_elements"] = new_graph_elements
-                return dashboard_graph_data
-            else:
-                if 'job_title' in tap_elements[0]['data']:
-                    tap_elements.append({'data': {'label': tap_elements[0]['data']['job_title']}})
-                dashboard_graph_data["graph_elements"] = current_elements + tap_elements
-            return dashboard_graph_data
-        else:
-            raise PreventUpdate
-    else:
+@cache.memoize(timeout=TIMEOUT)
+def handle_tap_node(tap_node_data, current_elements, dashboard_graph_data):
+    if not tap_node_data:
         raise PreventUpdate
 
+    node_id = tap_node_data['id']
+    id_to_data = dashboard_graph_data.get("id_to_data", {})
+    id_to_children = dashboard_graph_data.get("id_to_children", {})
 
-def get_children_ids(start_id: str, id_to_children: dict):
+    tap_elements = id_to_data.get(node_id, [])
+    if len(tap_elements) <= 1:
+        raise PreventUpdate
+
+    if any(elem in current_elements for elem in tap_elements[1:]):
+        children_ids = get_children_ids(node_id, id_to_children)
+        elements_to_remove = [elem for cid in children_ids for elem in id_to_data.get(cid, [])]
+        elements_to_remove.pop(0)
+        new_graph_elements = [elem for elem in current_elements if elem not in elements_to_remove]
+        return new_graph_elements
+    else:
+        if 'job_title' in tap_elements[0]['data']:
+            tap_elements.append({'data': {'label': tap_elements[0]['data']['job_title']}})
+    return current_elements + tap_elements
+
+
+@cache.memoize(timeout=TIMEOUT)
+def get_children_ids(start_id: str, id_to_children: dict) -> list:
     result = []
 
     def dfs(current_id: str):
--- visual_organizational_structure/utils.py
+++ visual_organizational_structure/utils.py
@@ -1,5 +1,7 @@
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_caching import Cache
 
 manager = LoginManager()
 mail = Mail()
+cache = Cache()
