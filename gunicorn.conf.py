b9c60a92696f9973d57c2bc5db9356d01595338a
Materacl <yurchenko.stas15@gmail.com>
2024-05-24 19:05:11 +0500

    Поменял класс воркеров в gunicorn

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,7 @@
 import multiprocessing
 
-worker_class = 'sync' # or gthread, gevent, tornado, eventlet, etc.
+worker_class = 'gthread' # or gthread, gevent, tornado, eventlet, etc.
 bind = '0.0.0.0:8080'
 timeout = '600'
+threads = 2
 workers = 2 * multiprocessing.cpu_count() + 1
